39fd15f83e323e48fa922a7e6e1d9a0dcba6c9f9
mariodac <mdac.mario@gmail.com>
2019-09-16 23:27:46 -0300

    Exercicio capitulo 3

--- exerciciosCap3.py
+++ exerciciosCap3.py
@@ -1,6 +1,7 @@
 from os import system, name
-from math import sqrt, acos, degrees
+from math import sqrt, acos, degrees, inf
 from fractions import Fraction
+from numpy import arange
 
 def produtoEscalar(vetorU, vetorV, tamanho):
     resultado = 0
@@ -23,18 +24,34 @@ def anguloEntreVetores(vetorU, vetorV, tamanho):
 
 def projecaoVetores(vetorU, vetorV, tamanho):
     escalar = produtoEscalar(vetorU, vetorV, tamanho)
-    comprimentoV = pow(comprimento(vetorU,tamanho), 2)
+    comprimentoV = pow(comprimento(vetorV,tamanho), 2)
     resultado = escalar / comprimentoV
     proj = list()
     for i in range(0, tamanho):
         proj.append(Fraction(resultado * vetorV[i]).limit_denominator())
     return proj
 
+def gerar_primos(quantidade, limite):
+    primos = list()
+    c = 0
+    primo = 0
+    for numero in range(2, limite+1):
+        for i in range(2,numero):
+            if numero % i == 0: break
+            c += 1
+        else:
+            if len(primos) == 50: break
+            primos.append(numero)
+            primo += 1
+    return primos
+
+
 def menu():
     tilt = "*"
     print("{}Exercícios Computacionais do capítulo 3{}".format(tilt*10, tilt*10))
     for i in range(1, 6):
         print("Exercicio {}".format(i))
+    print("Digite 6 para exercicio 1 ao 4")
     print("0 para sair")
 
 if __name__ == "__main__":
@@ -66,7 +83,7 @@ if __name__ == "__main__":
             vetorU = list()
             for i in range(0, tam):
                 vetorU.append(int(input("Digite a coordenada {} do vetor u: ".format(i+1))))
-            print("Comprimento do vetor u: {}".format(comprimento(vetorU, tam)))
+            print("Comprimento do vetor u: {}".format(round(comprimento(vetorU, tam), 2)))
         elif op == 3:
             system('cls' if name == 'nt' else 'clear')
             print("Exercicio 3")
@@ -76,7 +93,7 @@ if __name__ == "__main__":
             for i in range(0, tam):
                 vetorU.append(int(input("Digite a coordenada {} do vetor u: ".format(i+1))))
                 vetorV.append(int(input("Digite a coordenada {} do vetor v: ".format(i+1))))
-            print("O angulo entre vetores é: {}°".format(anguloEntreVetores(vetorU, vetorV, tam)))
+            print("O angulo entre vetores é: {}°".format(round(anguloEntreVetores(vetorU, vetorV, tam),2)))
         elif op == 4:
             system('cls' if name == 'nt' else 'clear')
             print("Exercicio 4")
@@ -96,6 +113,43 @@ if __name__ == "__main__":
         elif op == 5:
             system('cls' if name == 'nt' else 'clear')
             print("Exercicio 5")
+            tam = 50
+            vetorU = gerar_primos(tam, 1000)
+            vetorV = list()
+            for i in range(0, tam):
+                vetorV.append(int(pow(-1, i)))
+            print("Produto Escalar =  {}".format(produtoEscalar(vetorU, vetorV, tam)))
+            print("Comprimento Vetor U = {}".format(round(comprimento(vetorU, tam), 2)))
+            print("Comprimento Vetor V = {}".format(round(comprimento(vetorV, tam), 2)))
+            print("Angulo entre vetor u e v = {}°".format(round(anguloEntreVetores(vetorU, vetorV, tam), 2)))
+            resultado = projecaoVetores(vetorU, vetorV, tam)
+            print("Projeção de vetor u em relação a v = (", end="")
+            for i in range(0, tam):
+                if(i == tam-1):
+                    print("{}".format(resultado[i]), end="")
+                else: print("{}, ".format(resultado[i]), end="")
+            print(")")
+        elif op == 6:
+            system('cls' if name == 'nt' else 'clear')
+            print("Exercicio 1 a 4")
+            tam = int(input("Digite o tamanho do vetor: "))
+            vetorU = list()
+            vetorV = list()
+            for i in range(0, tam):
+                vetorU.append(int(input("Digite a coordenada {} do vetor u: ".format(i+1))))
+                vetorV.append(int(input("Digite a coordenada {} do vetor v: ".format(i+1))))
+            print("Comprimento do vetor u: {}".format(comprimento(vetorU, tam)))
+            print("Comprimento do vetor v: {}".format(comprimento(vetorV, tam)))
+            print("Produto escalar = ", produtoEscalar(vetorU, vetorV, tam))
+            print("O angulo entre vetores é: {}°".format(anguloEntreVetores(vetorU, vetorV, tam)))
+            resultado = projecaoVetores(vetorU, vetorV, tam)
+            print("Projeção de vetor u em relação a v = (", end="")
+            for i in range(0, tam):
+                if(i == tam-1):
+                    print("{}".format(resultado[i]), end="")
+                else: print("{}, ".format(resultado[i]), end="")
+            print(")")
+                
         else:
             system('cls' if name == 'nt' else 'clear')
             print("Opção inválida")
